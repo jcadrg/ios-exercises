9b21c1ffc4214d3d682732182539d06ca58f6cea
Carlos Gomez <jcadrg@gmail.com>
2015-05-20 23:52:38 -0400

    Strings Assignment Completed

--- BlocExercises/BlocExercises/StringCheese.m
+++ BlocExercises/BlocExercises/StringCheese.m
@@ -11,27 +11,43 @@
 @implementation StringCheese
 
 - (NSString *) favoriteCheeseStringWithCheese:(NSString *)cheeseName {
-    /* WORK HERE */
-    return nil;
+    NSString *favoriteCheese = [NSString stringWithFormat:@"My favorite cheese is %@.", cheeseName];
+    
+    
+    return (favoriteCheese);
 }
 
 - (NSString *) cheeseNameWithoutCheeseSuffix:(NSString *)cheeseName {
-    /* WORK HERE */
-    return nil;
+    NSString *resultString;
+    
+    NSRange cheeseRange = [cheeseName rangeOfString:@" CheEse" options:NSCaseInsensitiveSearch];
+    if (cheeseRange.location == NSNotFound) {
+        resultString = (cheeseName);
+    } else{
+    
+    NSString * shortName = [cheeseName stringByReplacingCharactersInRange:cheeseRange withString:@""];
+    resultString = (shortName);
+    
+    }
+    return resultString;
 }
 
 - (NSString *) numberOfCheesesStringWithCheeseCount:(NSUInteger)cheeseCount {
+//    return [NSString stringWithFormat:@"%lu %@",(unsigned long)cheeseCount,(cheeseCount == 1)?@"cheese":@"cheeses"];
+ 
+    NSMutableString *resultNumberString =[[NSString stringWithFormat:@"%lu",(unsigned long)cheeseCount] mutableCopy];
     if (cheeseCount == 1) {
-        /* WORK HERE, ASSUMING THERE IS 1 CHEESE */
+        
+        [resultNumberString appendString:@" cheese"];
+
     } else {
-        /* WORK HERE, ASSUMING THERE ARE 2+ CHEESES */
+        [resultNumberString appendString:@" cheeses"];
     }
     
     /*
      (You will learn more about if/else statements in the next checkpoint.)
      */
-    
-    return nil;
+    return resultNumberString;
 }
 
 @end
