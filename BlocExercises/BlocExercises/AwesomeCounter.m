046ab027fb0064bf8992e0e3a77bc625b1d2ba6f
Carlos Gomez <jcadrg@gmail.com>
2015-05-22 18:15:08 -0400

    Completed loops assginment

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,35 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+    NSMutableString * numberStringResult;
+    
+    if (number>otherNumber) {
+        
+        NSMutableString * numberStringResult1 =[[NSString stringWithFormat:@"%d",(int)otherNumber]mutableCopy];
+        for (NSInteger x=otherNumber+1; x<=number; x++) {
+            
+            [numberStringResult1 appendString:[NSString stringWithFormat:@"%d", (int)x]];
+            
+        }
+        numberStringResult=numberStringResult1;
+    
+    }else if (number<otherNumber){
+        
+        NSMutableString * numberStringResult2 =[[NSString stringWithFormat:@"%d",(int)number]mutableCopy];
+        
+        for (NSInteger x=number+1; x<=otherNumber; x++) {
+            
+            [numberStringResult2 appendString:[NSString stringWithFormat:@"%d", (int)x]];
+            
+        }
+        numberStringResult=numberStringResult2;
+    
+    }else if(number==otherNumber){
+        NSMutableString * numberStringResult3 =[[NSString stringWithFormat:@"%d",(int)number]mutableCopy];
+        numberStringResult=numberStringResult3;
+    }
+    
+    return numberStringResult;
 }
 
 @end
